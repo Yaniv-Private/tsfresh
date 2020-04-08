429524a9ad56316b4ed9cbe8b5532fd00931c9a9
Derek Snow <d.snow@jbs.cam.ac.uk>
2020-04-08 12:17:08 +1200

    Update extraction.py

--- tsfresh/feature_extraction/extraction.py
+++ tsfresh/feature_extraction/extraction.py
@@ -358,6 +358,7 @@ def _do_extraction_on_chunk(chunk, df, default_fc_parameters, kind_to_fc_paramet
             for feat, _ in fc_parameters[key].items():
                 print(str(len(df)))
                 dict_param[feat] = df[(df["ID"]==sample_id)&(df["_variables"]==feat)]["_values"]
+                print(dict_param[feat].head())
             fc_parameters["var_index"] = [dict_param[feat]]
         else:
             print("No Double Feature") 
